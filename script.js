82eba664c23fd436f381e5e6c0e906829afbf43d
Adam Orlowski <adamorlowskipoland@icloud.com>
2017-04-20 01:17:36 +0200

    set up needed files

---
+++ script.js
@@ -0,0 +1,32 @@
+const model = {
+    "pics": [
+        {
+            "imgName" : "pic1",
+            "imgSrc" : "imgs/pic1.jpg",
+            "imgAlt" : "Img training 1"
+        },
+        {
+            "imgName" : "pic2",
+            "imgSrc" : "imgs/pic2.jpg",
+            "imgAlt" : "Img training 2"
+        },
+        {
+            "imgName" : "pic3",
+            "imgSrc" : "imgs/pic3.jpg",
+            "imgAlt" : "Img training 3"
+        },
+        {
+            "imgName" : "pic4",
+            "imgSrc" : "imgs/pic4.jpg",
+            "imgAlt" : "Img training 4"
+        }
+    ]
+}
+
+const operator = {
+
+}
+
+const view = {
+    
+}
